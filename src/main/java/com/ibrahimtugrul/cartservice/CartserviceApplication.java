14f4a512831cb6acc300293b99a4f1e0206e7798
İbrahim Tuğrul <ibrahimtugrul@Ibrahim-MacBook-Pro.local>
2020-06-08 05:11:47 +0300

    add swagger

--- src/main/java/com/ibrahimtugrul/cartservice/CartserviceApplication.java
+++ src/main/java/com/ibrahimtugrul/cartservice/CartserviceApplication.java
@@ -1,14 +1,7 @@
 package com.ibrahimtugrul.cartservice;
 
-import com.ibrahimtugrul.cartservice.domain.entity.Product;
-import com.ibrahimtugrul.cartservice.domain.repository.ProductRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
-import java.util.Currency;
 
 @SpringBootApplication
 public class CartserviceApplication {
@@ -17,11 +10,4 @@ public class CartserviceApplication {
 		SpringApplication.run(CartserviceApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner runner(ProductRepository productRepository) {
-		return args -> {
-			productRepository.save(Product.builder().categoryId(0L).price(25).title("init").build());
-		};
-
-	}
 }
--- src/main/java/com/ibrahimtugrul/cartservice/infrastructure/configuration/WebConfig.java
+++ src/main/java/com/ibrahimtugrul/cartservice/infrastructure/configuration/WebConfig.java
@@ -14,8 +14,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
-import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 import javax.servlet.Filter;
 
